92a32eb49d8c37cbe6af3fbb7bab7d62d77856d5
nschaetti <n.schaetti@gmail.com>
2021-01-02 10:17:47 +0100

    Basics

---
+++ tutorials/basics.py
@@ -0,0 +1,27 @@
+
+from manimlib.imports import *
+
+class Shapes(Scene):
+
+    # Construct the scene
+    def construct(self):
+        """
+        Construct the scene
+        :return:
+        """
+        circle = Circle()
+        square = Square()
+        triangle = Polygon(
+            np.array([0, 0, 0]),
+            np.array([1, 1, 0]),
+            np.array([1, -1, 0])
+        )
+
+        # Showing shapes
+        self.play(ShowCreation(circle))
+        self.play(FadeOut(circle))
+        self.play(GrowFromCenter(square))
+        self.play(Transform(square, triangle))
+    # end construct
+
+# end Shapes
